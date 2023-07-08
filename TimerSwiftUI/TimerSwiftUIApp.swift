14eb18b59977f461d686fd384598afa5611b00ad
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-07-08 18:21:42 +0500

    Initial Commit

---
+++ TimerSwiftUI/TimerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerSwiftUIApp.swift
+//  TimerSwiftUI
+//
+//  Created by sss on 08.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TimerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
